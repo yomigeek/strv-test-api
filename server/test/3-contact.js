62da4eca4f42e8f4099c3202efb4f2044f4d8c88
yomigeek <seayomi@gmail.com>
2021-05-06 16:54:58 +0100

    set up test

--- server/test/3-contact.js
+++ server/test/3-contact.js
@@ -7,12 +7,35 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 
+let token;
+
 // Test add contact API/functions
 describe("User Add Contact API Tests", () => {
+  it("should login a user account on /api/v1/auth/login POST", (done) => {
+    chai
+      .request(app)
+      .post("/api/v1/auth/login")
+      .send({
+        email: "yomi@example.com",
+        password: "12345678",
+      })
+      .end((err, res) => {
+        token = res.body.data.token;
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a("object");
+        res.body.should.have.property("status");
+        res.body.should.have.property("data");
+        res.body.data.should.have.property("token");
+        res.body.status.should.equal("success");
+        done();
+      });
+  });
   it("should signup a user account on /api/v1/contact/add POST", (done) => {
     chai
       .request(app)
       .post("/api/v1/contact/add")
+      .set("x-access-token", token)
       .send({
         email: "yomi@example.com",
         password: "12345678",
